bde93ad977f81510872556a91e175423ad08cbf3
qqqq74ta <95514246+qqqq74ta@users.noreply.github.com>
2023-03-11 13:33:20 +0800

    Update ClockIn.py

--- ClockIn.py
+++ ClockIn.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
     time.sleep(3)
     now_H = int(time.strftime("%H"))
     if flag:
-        if 14 <= now_H <= 15:  # 世界协调时间
+        if 12 <= now_H <= 15:  # 世界协调时间
             sign_in_evening(token)
         else:
             sign_in(token)
